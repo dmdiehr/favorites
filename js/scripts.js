99256f9497d522bf3aae1329994f49b329b1764a
dmdiehr <ddiehr@yahoo.com>
2015-10-09 20:46:30 -0700

    Adjust margin/padding of display sectioni, add background create background images to be used later.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+  
+  // Change CSS background image to correspond with each list category
+  //body {
+  // background-image: url("../images/music.jpg");
+  // background-repeat: no-repeat;
+  // background-position: center;
+  //}
