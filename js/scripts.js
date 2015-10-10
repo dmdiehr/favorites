91d73b6c84172ba183cb7aea1ba43e340b28c824
dmdiehr <ddiehr@yahoo.com>
2015-10-10 01:22:00 -0700

    Add the js and jq so the submit button will update the contents of the made list, and change the background image to reflect the category.

--- js/scripts.js
+++ js/scripts.js
@@ -19,19 +19,56 @@ $(document).ready(function() {
 
   // Form Buttons Functionalities 
 
-  $("#button1id").click(function(){
-    // var category = $("#selectbasic").val();
-    // var firstInputValue = $("input#first-input").val();
-    // var secondInputValue = $("input#second-input").val();
-    // var thirdInputValue = $("input#third-input").val();
-    // var fourthInputValue = $("input#fourth-input").val();
-    // var fifthInputValue = $("input#fifth-input").val();
+  $("#button1id").click(function(event){
+    var categoryInputValue = $("#selectbasic").val();
+    var firstInputValue = $("input#first-input").val();
+    var secondInputValue = $("input#second-input").val();
+    var thirdInputValue = $("input#third-input").val();
+    var fourthInputValue = $("input#fourth-input").val();
+    var fifthInputValue = $("input#fifth-input").val();
+   
+    if (categoryInputValue == 1) {
+      categoryString = 'Any';
+      $('body').css('background-image', 'none');
+    };
+    if (categoryInputValue == 2) {
+      categoryString = 'Musical Acts';
+      $('body').css('background-image', 'url(images/music.jpg)');
 
-    $("#form-section").hide();
-    $("#display-section").show();
-    $("#first-list").hide();
-    $("#made-list").show();
-    event.preventDefault();
+    };
+    if (categoryInputValue == 3) {
+      categoryString = 'Sports Teams';
+      $('body').css('background-image', 'url(images/sports.jpg)');
+    };
+    if (categoryInputValue == 4) {
+      categoryString = 'Books';
+      $('body').css('background-image', 'url(images/books.jpg)');
+    };
+    if (categoryInputValue == 5) {
+      categoryString = 'Movies';
+      $('body').css('background-image', 'url(images/movies.jpg)');
+    };
+    if (categoryInputValue == 6) {
+      categoryString = 'Websites';
+      $('body').css('background-image', 'url(images/internet.jpg)');
+    };
+
+    if (firstInputValue !=='') {
+
+      $('#category').text(categoryString);
+      $("#first-li").text(firstInputValue);
+      $("#second-li").text(secondInputValue);
+      $("#third-li").text(thirdInputValue);
+      $("#fourth-li").text(fourthInputValue);
+      $("#fifth-li").text(fifthInputValue);
+
+
+      $("#form-section").hide();
+      $("#display-section").show();
+      $("#first-list").hide();
+      $("#made-list").show();
+      event.preventDefault();
+    };
   }); // ends submit button function
 
   $("#button2id").click(function(event){
